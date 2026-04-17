742f1473659cdde5b7ef043a86082168d89a2d3c
Christian Lopez <chydals@gmail.com>
2026-04-17 17:02:23 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MainApp from './src/MainApp';
+import './App.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>
+);
