64afffddcf0d4bab64d2082b04b5e455f46cbaaa
Christian Lopez <chydals@gmail.com>
2026-04-20 14:13:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,4 @@ root.render(
   <React.StrictMode>
     <MainApp />
   </React.StrictMode>
-);import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MainApp from './src/MainApp'; // Added /src/ here
-import './App.css'; 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <MainApp />
-  </React.StrictMode>
 );
