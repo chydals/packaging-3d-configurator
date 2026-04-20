ed74a12ac3c57de9a27c0620d25418d3e98c7700
Christian Lopez <chydals@gmail.com>
2026-04-20 14:33:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MainApp from './src/MainApp'; // This tells it to go into the src folder
+import MainApp from './MainApp';
 import './App.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
